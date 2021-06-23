3bcfbe7c25f99f83a4debc659b7c2539a0ae6254
Your Name <you@example.com>
2021-06-23 10:01:06 +0530

    adding react bootstrap

--- src/components/RestaurantsList.js
+++ src/components/RestaurantsList.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class RestaurantsList extends Component {
+    constructor(){
+        super();
+        this.state ={
+            list:null
+        }
+    }
+    componentDidMount(){
+        axios.get("http://localhost:3000/restaurant")
+        .then(response=>{
+            this.setState({list:response.data})
+        });
+    }
     render() {
+        console.log(this.state);
+
         return (
-            <div>
-                <h1>Restaurants List</h1>
-            </div>
+          <div>
+            <h1>Restaurants List</h1>
+            {this.state.list ? (
+              <div>
+                {this.state.list.map((item, i) => (
+                  <div key={i} className="list-wrapper">
+                    <span>{item.name}</span>
+                    <span>{item.email}</span>
+                    <span>{item.rating}</span>
+                    <span>{item.address}</span>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p>Please Wait</p>
+            )}
+          </div>
         );
     }
 }
